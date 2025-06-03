ac1527efbb4d4c4f721807172516e4750c67cda8
Nathene <nathanvisnjic@gmail.com>
2025-06-03 18:05:46 +1000

    modular db

--- nhensby.com/lib/nhensby/repo.ex
+++ nhensby.com/lib/nhensby/repo.ex
@@ -1,4 +1,5 @@
 defmodule Nhensby.Repo do
   use Ecto.Repo,
-    otp_app: :nhensby
+    otp_app: :nhensby,
+    adapter: if(Mix.env() == :prod, do: Ecto.Adapters.SQLite3, else: Ecto.Adapters.Postgres)
 end
